c452af7167afd51eb894261ff7971371476b5b2a
Keshtech2002 <keshtech2002@gmail.com>
2023-10-11 22:42:28 +0100

    introducing to coding in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	/*string to display*/
+	char string[] = "Programming is like building a multilingual puzzle";
+	
+	/*display the string*/
+	puts(string);
+
+	return 0;
+}
